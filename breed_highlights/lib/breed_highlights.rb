816a01fbef84241c8f2b9b6b46cd43801e039d72
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-12-05 23:59:39 +0000

    pokusaj

--- breed_highlights/lib/breed_highlights.rb
+++ breed_highlights/lib/breed_highlights.rb
@@ -1,6 +1,7 @@
 require_relative "./breed_highlights/version"
 require_relative "breed_highlights/cli"
 
+require 'pry'
 
 module BreedHighlights
   class Error < StandardError; end
--- breed_highlights/lib/breed_highlights/cli.rb
+++ breed_highlights/lib/breed_highlights/cli.rb
@@ -2,6 +2,7 @@ class BreedHighlights::CLI
 
   def call 
     puts "Welcome to the app about our best friends - dogs!"
+    binding.pry
   end
   
 end
